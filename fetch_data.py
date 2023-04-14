01367767d64dba0e7b380dc90914ef45dcd00e31
brockmiddleton <brockmiddleton4@gmail.com>
2023-04-14 12:32:53 -0600

    Add PCA and PolyTransformation to data fetcher

--- fetch_data.py
+++ fetch_data.py
@@ -3,17 +3,15 @@ import numpy as np
 import sys
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
-def get_walmart_data(std, dimen_reduc):
+def import_data_walmart():
     """
-    Import and perform data manipulation on Walmart dataset
-    Returns a tuple of the following values:
-        X_train,
-        X_test,
-        y_train,
-        y_test
+    Import Walmart dataset and perform necessary data manipulation
+    Returns a clean 'train' and 'target' set
     """
-
     # Import data
     features = pd.read_csv('../walmart-recruiting-store-sales-forecasting/features/features.csv')
     train = pd.read_csv('../walmart-recruiting-store-sales-forecasting/train/train.csv')
@@ -25,7 +23,7 @@ def get_walmart_data(std, dimen_reduc):
     # Merge stores w features
     feat_stores = features.merge(stores, how='inner', on = "Store")
 
-    # Convvert Dates to date datatypes
+    # Convert Data features to corresponding datatypes
     feat_stores['Date'] = pd.to_datetime(feat_stores['Date'])
     feat_stores['Day'] = feat_stores['Date'].dt.day
     feat_stores['Week'] = feat_stores['Date'].dt.week
@@ -33,7 +31,7 @@ def get_walmart_data(std, dimen_reduc):
     feat_stores['Year'] = feat_stores['Date'].dt.year
     feat_stores['WeekOfYear'] = (feat_stores.Date.dt.isocalendar().week)*1.0 
 
-    # Merge feat_stores w train and test data
+    # Merge feat_stores w training and testing data
     train_df = train.merge(feat_stores, how='inner', on = ['Store','Date','IsHoliday']).sort_values(by=['Store','Dept','Date']).reset_index(drop=True)
     test_df = test.merge(feat_stores, how='inner', on = ['Store','Date','IsHoliday']).sort_values(by = ['Store','Dept','Date']).reset_index(drop=True)
 
@@ -53,31 +51,15 @@ def get_walmart_data(std, dimen_reduc):
     train_cols.remove('Weekly_Sales')
     target_col = 'Weekly_Sales'
 
-    train_df = interm_train[train_cols].copy()
-    test_df = interm_train[target_col].copy()
+    train = interm_train[train_cols].copy()
+    target = interm_train[target_col].copy()
 
-    # if (std):
-    # STANDARDIZE
+    return[train, target]
 
-    # if (dimen_reduc:
-    # DIMENSIONALITY REDUCTION
-
-    # Split Data
-    # NOTE: NOT STANDARDIZED 
-    X_train, X_test, y_train, y_test = train_test_split(
-        train_df, test_df, test_size=0.3, random_state=1
-    )
-
-    return [X_train, X_test, y_train, y_test]
-
-def get_rossmann_data(std, dimen_reduc):
+def import_data_rossmann():
     """
-    Import and perform data manipulation on Rossmann dataset
-    Returns a tuple of the following values:
-        X_train,
-        X_test,
-        y_train,
-        y_test
+    Import Rossmann dataset and perform necessary data manipulation
+    Returns a clean 'train' and 'target' set
     """
 
     # Import data
@@ -100,7 +82,7 @@ def get_rossmann_data(std, dimen_reduc):
     train['StateHoliday'] = pd.to_numeric(train['StateHoliday'])
     store=store.drop('PromoInterval', axis=1)
 
-    # Merge 'store' data with training and testing 
+    # Merge store data with training and testing 
     train_store = train.merge(store, how='inner', on="Store")
     test_store = test.merge(store, how='inner', on="Store")
 
@@ -118,7 +100,9 @@ def get_rossmann_data(std, dimen_reduc):
     train_df = train_store
     test_df = test_store
 
+    test_df=test_df.drop('Date', axis=1)
     test_df=test_df.drop('Id', axis=1)
+    train_df=train_df.drop('Date', axis=1)
 
     # Fill NaN
     train_df = train_df.fillna(0)
@@ -131,28 +115,149 @@ def get_rossmann_data(std, dimen_reduc):
     train = train_df[train_cols].copy()
     target = train_df['Sales'].copy()
 
-    # if (std):
-    # STANDARDIZE
+    return[train, target]
 
-    # if (dimen_reduc:
-    # DIMENSIONALITY REDUCTION
+def normalize(training_data, testing_data):
+    """
+    Perform normalization on given training data using StandardScaler
+    """
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        train, target, test_size=0.3, random_state=1
-    )
+    sc = StandardScaler()
+    train = sc.fit_transform(training_data)
+    test = sc.transform(testing_data)
+    
+    return[train, test]
 
-    X_train = X_train.drop('Date', axis=1)
-    X_test = X_test.drop('Date', axis=1)
+def transform(training_data, testing_data, n_degrees):
+        
+        poly = PolynomialFeatures(degree=n_degrees, include_bias=False)
+        train_tr = poly.fit_transform(training_data)
+        test_tr = poly.transform(testing_data)
 
-    return [X_train, X_test, y_train, y_test]
+
+        return[train_tr, test_tr]
+
+def get_pca(train_std, test_std, n_components):
+    """
+    Perform dimensionality reduction using Principal Component Analysis on given data
+    """
+    pca = PCA(n_components=n_components)
+    train_pca = pca.fit_transform(train_std)
+    test_pca = pca.transform(test_std)
+
+    return[train_pca, test_pca]
 
 class data_fetcher:
 
-    def __init__(self,dataset='walmart', std=False, dimen_reduc='None'):
+    def __init__():
+        return
+    
+    def fetch_walmart(norm, pca, n_components, n_degrees):
 
-        if (dataset == 'walmart'):
-            self.data = get_walmart_data(std,dimen_reduc)
+        train, target = import_data_walmart()
 
-        elif (dataset == 'rossmann'):
-            self.data = get_rossmann_data(std,dimen_reduc)
+        ## Transformation
+        if (n_degrees!=1):
 
+            # Split
+            X_train, X_test, y_train, y_test = train_test_split(
+                train, target, test_size=0.3, random_state=1
+            )
+
+            X_train_tr, X_test_tr = transform(X_train, X_test, n_degrees)
+
+            X_train_std, X_test_std = normalize(X_train_tr, X_test_tr)
+            
+            return[X_train_std, X_test_std, y_train, y_test]
+
+
+
+        ## Exception - no PCA without norm
+        if (not norm and pca):
+            raise Exception('Cannot perform Principal Component Analysis on non-normalized data.')
+        
+        ## no Normalization, no PCA
+        elif (not norm and not pca):
+
+            # Split
+            X_train, X_test, y_train, y_test = train_test_split(
+                train, target, test_size=0.3, random_state=1
+            )
+            return[X_train, X_test, y_train, y_test]
+
+        ## Normalization, no PCA
+        elif (norm and not pca):
+
+            # Split
+            X_train, X_test, y_train, y_test = train_test_split(
+                train, target, test_size=0.3, random_state=1
+            )
+
+            X_train_std, X_test_std = normalize(X_train, X_test)
+            
+            return[X_train_std, X_test_std, y_train, y_test]
+        
+        ## Normalization and PCA
+        elif (norm and pca):
+
+            # Split
+            X_train, X_test, y_train, y_test = train_test_split(
+                train, target, test_size=0.3, random_state=1
+            )
+
+            X_train_std, X_test_std = normalize(X_train, X_test)
+
+            # PCA
+            train_pca, test_pca = get_pca(X_train_std, X_test_std, n_components=n_components)
+
+            return[train_pca, test_pca, y_train, y_test]
+    
+    def fetch_rossmann(norm, pca, n_components, n_degrees):
+
+        train, target = import_data_rossmann()
+
+        ## Transformation
+        if (n_degrees!=1):
+
+            train = transform(train, n_degrees)
+
+        ## Exception - no PCA without norm
+        if (not norm and pca):
+            raise Exception('Cannot perform Principal Component Analysis on non-normalized data.')
+        
+        ## no Normalization, no PCA
+        elif (not norm and not pca):
+
+            # Split
+            X_train, X_test, y_train, y_test = train_test_split(
+                train, target, test_size=0.3, random_state=1
+            )
+            return[X_train, X_test, y_train, y_test]
+
+        ## Normalization, no PCA
+        elif (norm and not pca):
+
+            # Split
+            X_train, X_test, y_train, y_test = train_test_split(
+                train, target, test_size=0.3, random_state=1
+            )
+
+            X_train_std, X_test_std = normalize(X_train, X_test)
+            
+            return[X_train_std, X_test_std, y_train, y_test]
+        
+        ## Normalization and PCA
+        elif (norm and pca):
+
+            # Split
+            X_train, X_test, y_train, y_test = train_test_split(
+                train, target, test_size=0.3, random_state=1
+            )
+
+            X_train_std, X_test_std = normalize(X_train, X_test)
+
+            # PCA
+            train_pca, test_pca = get_pca(X_train_std, X_test_std, n_components=n_components)
+
+            return[train_pca, test_pca, y_train, y_test]
+        return

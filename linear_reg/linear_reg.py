4ac09d416a480eb23e46f9d55eae57e98f58794d
brockmiddleton <brockmiddleton4@gmail.com>
2023-04-21 12:31:46 -0600

    Update LR py

--- linear_reg/linear_reg.py
+++ linear_reg/linear_reg.py
@@ -12,7 +12,7 @@ class linear_reg:
 
         return
     
-    def get_linear_regression():
+    def get_linear_regression(X_train, X_test, y_train, y_test):
 
         ## NO MANIP
         lr = LinearRegression()
@@ -24,23 +24,13 @@ class linear_reg:
         pred_train = lr.predict(X_train)
         pred_test = lr.predict(X_test)
 
-        ## STANDARDIZED 
-        lr = LinearRegression()
-        st = time.time()
-        lr.fit(X_train_std, y_train_std)
-        en = time.time()
-        fit_time_std = (en-st)
-
-        pred_train_std = lr.predict(X_train_std)
-        pred_test_std = lr.predict(X_test_std)
-
-
         ## R2
         r2_train_lr = r2_score(y_train, pred_train)
         r2_test_lr = r2_score(y_test, pred_test)
 
-        r2_train_std_lr = r2_score(y_train_std, pred_train_std)
-        r2_test_std_lr = r2_score(y_test_std, pred_test_std)
+        ## MSE
+        mse_train_lr = mean_squared_error(y_train, pred_train)
+        mse_test_lr = mean_squared_error(y_test, pred_test)
 
         ## TIME
         print('Fit Times:\n--------------------\n')
@@ -50,17 +40,8 @@ class linear_reg:
         print("\nR2 Scores\n---------------------\n")
         print("R2 (train) for %s:             %0.5f%% " % ('LR', r2_train_lr))
         print("R2 (test) for %s:              %0.5f%% " % ('LR', r2_test_lr))
-        print("R2 (train - STD) for %s:       %0.5f%% " % ('LR', r2_train_std_lr))
-        print("R2 (test - STD) for %s:        %0.5f%% \n\n" % ('LR', r2_test_std_lr))
 
-        ## MSE
-        mse_train_lr = mean_squared_error(y_train, pred_train)
-        mse_test_lr = mean_squared_error(y_test, pred_test)
-        mse_train_std_lr = mean_squared_error(y_train_std, pred_train_std)
-        mse_test_std_lr = mean_squared_error(y_test_std, pred_test_std)
 
         print("MSE Scores\n---------------------\n")
         print("MSE (train) for %s:              %0.5f%% " % ('LR', mse_train_lr))
         print("MSE (test) for %s:               %0.5f%% " % ('LR', mse_test_lr))
-        print("MSE (train - STD) for %s:        %0.5f%% " % ('LR', mse_train_std_lr))
-        print("MSE (test - STD) for %s:         %0.5f%% " % ('LR', mse_test_std_lr))
